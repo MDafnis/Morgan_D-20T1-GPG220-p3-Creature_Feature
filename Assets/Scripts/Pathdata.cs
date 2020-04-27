26aa0850e859231e241d5ce2ebabbe3d6a62dc43
Morgan Dafnis <1015103@student.sae.edu.au>
2020-04-27 12:08:58 +1000

    Proc Gen + my changes.
    - Settlement areas now spawn with different size and they also spawn a (shelter)
    - Trees and settlements are regenerated every time you press G.

---
+++ Assets/Scripts/Pathdata.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathdataNode
+{
+    Vector2Int GridLocation;
+    Vector3 WorldLocation;
+
+    public PathdataNode(int x, int z, Vector3 nodePosition)
+    {
+        GridLocation = new Vector2Int(x, z);
+        WorldLocation = nodePosition;
+    }
+}
+
+public class Pathdata : MonoBehaviour
+{
+    public Vector2Int WorldSize;
+    public List<PathdataNode> AllNodes = new List<PathdataNode>();
+
+    public bool DEBUG_DrawPathdata = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DEBUG_DrawPathdata)
+        {
+            
+        }
+    }
+
+    public PathdataNode CreateNode(int x, int z, Vector3 nodePosition)
+    {
+        PathdataNode newNode = new PathdataNode(x, z, nodePosition);
+
+        AllNodes.Add(newNode);
+
+        return newNode;
+    }
+}
--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TerrainGenerator : MonoBehaviour
 {
     [Header("Feature Generation")]
+    public int NumPasses = 5;
     public float FeatureNoise_X = 8f;
     public float FeatureNoise_Z = 8f;
     public float FeatureNoise_Amplitude = 0.5f;
@@ -13,6 +14,10 @@ public class TerrainGenerator : MonoBehaviour
     public float TextureNoise_X = 16f;
     public float TextureNoise_Z = 16f;
     public float TextureNoise_Amplitude = 0.5f;
+
+    [Header("Sphere List")]
+    private int originalSphereList = 174;
+    public List<GameObject> sphereList = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +29,18 @@ public class TerrainGenerator : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (sphereList.Count > 0)
+            {
+                foreach (GameObject gameObjects in sphereList)
+                {
+                    Destroy(gameObjects);
+                }
+                sphereList.Clear();
+            }
+
             GenerateTerrain();
+        }
     }
 
     void GenerateTerrain()
@@ -39,21 +55,40 @@ public class TerrainGenerator : MonoBehaviour
 		// retrieve the terrain
 		Terrain terrain = gameObject.GetComponent<Terrain>();
 
+        FindObjectOfType<Pathdata>().WorldSize = new Vector2Int(terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight);
+
 		// grab the height information
 		float[,] terrainHeights = new float[terrain.terrainData.heightmapWidth,terrain.terrainData.heightmapHeight];
 
 		int width = terrain.terrainData.heightmapWidth;
 		int height = terrain.terrainData.heightmapHeight;
 
+        //terrain.terrainData.heightmapScale
+
 		// set some initial height data
-		for (int x = 0; x < width; ++x)
-		{
-			for(int z = 0; z < height; ++z)
-			{
-				// apply noise
-				terrainHeights[x, z] = FeatureNoise_Amplitude * Mathf.PerlinNoise(FeatureNoise_X * x / width, FeatureNoise_Z * z / height);
-			}
-		}
+        for (int pass = 0; pass < NumPasses; ++pass)
+        {
+            for (int x = 0; x < width; ++x)
+            {
+                for(int z = 0; z < height; ++z)
+                {
+                    // apply noise
+                    if (pass == 0)
+                        terrainHeights[x, z] = FeatureNoise_Amplitude * Mathf.PerlinNoise(FeatureNoise_X * x / width, FeatureNoise_Z * z / height);
+                  //else if (pass == 1)
+                  //       {
+                  // terrainHeights[x, z] += Random.Range(-0.01f, 0.01f);                        
+                  //       }
+                    else
+                    {
+                        terrainHeights[x, z] += 0.01f * FeatureNoise_Amplitude * Mathf.PerlinNoise(FeatureNoise_X * x / width, FeatureNoise_Z * z / height);
+                    }
+                }
+            }
+        }
+
+        // loop 10 times
+        // pick random x and z (within the heightmap)
 
 		// update the heights
 		terrain.terrainData.SetHeights(0, 0, terrainHeights);        
@@ -74,12 +109,91 @@ public class TerrainGenerator : MonoBehaviour
 		int height = terrain.terrainData.alphamapHeight;
         int numLayers = terrain.terrainData.alphamapLayers;
 
-		for (int x = 0; x < width; ++x)
+        Vector2Int blockoutLocation = new Vector2Int(75, 75);
+        int blockOutRadius = 25;
+        int blockoutRadiusSq = blockOutRadius * blockOutRadius;
+
+        Vector2Int waterShelterLocation = new Vector2Int(35, 35);
+        int waterShelterRadius = 8;
+        int waterShelterRadiusSq = waterShelterRadius * waterShelterRadius;
+
+        GameObject waterShelterSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        waterShelterSphere.transform.localScale = new Vector3(10, 10, 10);
+        waterShelterSphere.transform.position = new Vector3Int(35, 1, 35);
+        sphereList.Add(waterShelterSphere);
+
+        Vector2Int storageShelterLocation = new Vector2Int(75, 35);
+        int storageShelterRadius = 10;
+        int storageShelterRadiusSq = storageShelterRadius * storageShelterRadius;
+
+        GameObject storageShelterSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        storageShelterSphere.transform.localScale = new Vector3(10, 10, 10);
+        storageShelterSphere.transform.position = new Vector3Int(35, 1, 74);
+        sphereList.Add(storageShelterSphere);
+
+        Pathdata pathdata = FindObjectOfType<Pathdata>();
+
+        for (int x = 0; x < width; ++x)
 		{
 			for(int z = 0; z < height; ++z)
 			{
+                Vector3 nodePosition = transform.position;
+                nodePosition = new Vector3(z * terrain.terrainData.heightmapScale.z, 
+                                            terrainHeights[x, z] * terrain.terrainData.heightmapScale.y,
+                                            x * terrain.terrainData.heightmapScale.x);   
+
+                PathdataNode node = pathdata.CreateNode(x, z, nodePosition);
+
+                int distToBlockoutSq = (x - blockoutLocation.x) * (x - blockoutLocation.x) + (z - blockoutLocation.y) * (z - blockoutLocation.y);
+                if (distToBlockoutSq < blockoutRadiusSq)
+                {
+                    // if (distToBlockoutSq > treeRadiusSq)
+                    // {
+                    //     // Do tree spawning with higher scale factor on the noise
+                    // }
+
+                    splatmapWeights[x, z, 0] = 0f;
+                    splatmapWeights[x, z, 1] = 0f;
+                    splatmapWeights[x, z, 2] = 1f;
+                    splatmapWeights[x, z, 3] = 0f;
+
+                    continue;
+                }
+
+                int distToWaterShelterSq = (x - waterShelterLocation.x) * (x - waterShelterLocation.x) + (z - waterShelterLocation.y) * (z - waterShelterLocation.y);
+                if (distToWaterShelterSq < waterShelterRadiusSq)
+                {
+                    // if (distToBlockoutSq > treeRadiusSq)
+                    // {
+                    //     // Do tree spawning with higher scale factor on the noise
+                    // }
+
+                    splatmapWeights[x, z, 0] = 0f;
+                    splatmapWeights[x, z, 1] = 1f;
+                    splatmapWeights[x, z, 2] = 0f;
+                    splatmapWeights[x, z, 3] = 0f;
+
+                    continue;
+                }
+
+                int distToStorageShelterSq = (x - storageShelterLocation.x) * (x - storageShelterLocation.x) + (z - storageShelterLocation.y) * (z - storageShelterLocation.y);
+                if (distToStorageShelterSq < waterShelterRadiusSq)
+                {
+                    // if (distToBlockoutSq > treeRadiusSq)
+                    // {
+                    //     // Do tree spawning with higher scale factor on the noise
+                    // }
+
+                    splatmapWeights[x, z, 0] = 0f;
+                    splatmapWeights[x, z, 1] = 1f;
+                    splatmapWeights[x, z, 2] = 0f;
+                    splatmapWeights[x, z, 3] = 0f;
+
+                    continue;
+                }
+
                 // below 20% of max height - force to be sand
-                if (terrainHeights[x, z] < 0.0175f)
+                if (terrainHeights[x, z] < 0.014f)
                 {
                     splatmapWeights[x, z, 0] = 1f;
                     splatmapWeights[x, z, 1] = 0f;
@@ -97,9 +211,20 @@ public class TerrainGenerator : MonoBehaviour
                 {
                     float grassWeight = TextureNoise_Amplitude * Mathf.PerlinNoise(TextureNoise_X * x / width, TextureNoise_Z * z / height);
 
+                    // would normally use different scale factors other than TextureNoise_
+                    float treeThreshold = 0.025f * Mathf.PerlinNoise(TextureNoise_X * x / width, TextureNoise_Z * z / height); // The block of this and below could potentially be used to spawn 
+                    if (Random.Range(0f, 1f) < treeThreshold)                                                                  // objects/structures within the world.
+                    {
+                        // Spawn an object
+                        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                        sphere.transform.position = nodePosition;
+                        sphereList.Add(sphere);
+
+                    }
+
                     splatmapWeights[x, z, 0] = 0f;
                     splatmapWeights[x, z, 1] = 1f - grassWeight;
-                    splatmapWeights[x, z, 2] = 0.2f + grassWeight; // grass
+                    splatmapWeights[x, z, 2] = 0.2f + grassWeight;
                     splatmapWeights[x, z, 3] = 0f;                    
                 }
 			}
